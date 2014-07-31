f1c1697a5038c7e3509ff009ec2e5ea243389ff5
Bernard Labno <bernard.labno@itcrowd.pl>
2014-07-31 18:36:30 +0200

    Import regex.js from https://github.com/rmurphey/js-assessment

---
+++ impl/regex.js
@@ -0,0 +1,35 @@
+(function ()
+{
+    'use strict';
+
+    window.regex = {
+        containsNumber: function (str)
+        {
+
+        },
+
+        containsRepeatingLetter: function (str)
+        {
+
+        },
+
+        endsWithVowel: function (str)
+        {
+
+        },
+
+        captureThreeNumbers: function (str)
+        {
+
+        },
+
+        matchesPattern: function (str)
+        {
+
+        },
+        isUSD: function (str)
+        {
+
+        }
+    };
+})();
