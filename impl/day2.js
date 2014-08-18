6958d7c24e778c6f3cf09b19220742b12455752d
Kronos66 <maksymilian.pamula@gmail.com>
2014-08-18 11:35:03 +0200

    Day2

--- impl/day2.js
+++ impl/day2.js
@@ -1,151 +1,10 @@
+/**
+ * Created by kronos on 14.08.14.
+ */
 (function () {
     'use strict';
-
+    var object =
     window.day2 = {
-        // Stworz funkcje wybierajaca pudelko (obiekty) o najwiekszej pojemnosci wedlug nastepujacych regul.
-        findTheBiggerBox: function (x, y, z) {
-            if (typeof x != "number" || typeof y != "number" || typeof z != "number" || x < 0 || y < 0 || z < 0) {
-                return false;
-            }
-            var boxX = {
-                sizeX: 3 * x,
-                sizeY: y,
-                sizeZ: z
-            };
-            var boxY = {
-                sizeX: x,
-                sizeY: y * y,
-                sizeZ: z
-            };
-            var boxZ = {
-                sizeX: x,
-                sizeY: y,
-                sizeZ: y + z
-            };
-            var boxes = [boxX, boxY, boxZ];
-            var size = function (box) {
-
-                return box.sizeX * box.sizeY * box.sizeZ;
-            }
-
-            var i = 0, nr = 0, maxSize = size(boxes[0]);
-            for (i = 1; i < boxes.length; i++) {
-                if (size(boxes[i]) > maxSize) {
-                    maxSize = size(boxes[i]);
-                    nr = i;
-
-                }
-            }
-            return nr + 1;
-        },
-        // Napisz funkcje zwracajaca obiekt reprezentujacy osobe. Funkcja ma przyjmowac: imie, nazwisko i wiek.
-        makePerson: function (firstName, lastName, age) {
-            var object = {firstName: firstName, lastName: lastName, age: age};
-            return object;
-        },
-        // Napisz funkcje pobierajaca string i zwracajaca go w odwrotnej kolejnosci
-        invertString: function (str) {
-            if (typeof str != "string") {
-                return false;
-            }
-            var invStr = "";
-            for (var i = 1; i <= str.length; i++) {
-                invStr += str.charAt(str.length - i)
-            }
-            return invStr;
-        },
-        // Napisz funkcje zwracajaca tablice ktorej wartosci sa wynikami silni danego indeksu
-        factorial: function (N) {
-            var tab = [];
-            tab.push(1);
-
-            for (var i = 1; i <= N; i++) {
-                tab.push(tab[i - 1] * i);
-            }
-            return tab;
-        },
-        // Napisz funkcje sumujaca parametry od 1 do 5 w podanej klasie przy uzyciu petli.
-        sumObject: function (obj) {
-            var sum = 0;
-            for (var i = 0; i < 5; i++) {
-                sum += obj["param" + (i)];
-            }
-            return sum;
-        },
-        //Napisz funkcje która wykorzytuje tablice wyrażeń funkcyjnych i modyfikuje podana tablice liczb mnożąc liczby
-        //o indeksach parzystych przez 3 ,a nieparzystych przez 4
-        multiplyElementOfArray: function (tab) {
-            var fun = [function (a) {
-                return a * 3;
-            }, function (a) {
-                return a * 4
-            }];
-            for (var i = 0; i < tab.length; i++) {
-                if (typeof tab[i] == "number") {
-                    if (i % 2 == 0) {
-
-                        tab[i] = fun[0](tab[i]);
-
-                    } else {
-
-                        tab[i] = fun[1](tab[i]);
-
-                    }
-                }
-
-            }
-            return tab;
-        },
-
-        //Napisz funkcje zwracajaca maksymalny element z tablicy
-        maxArray: function (tab) {
-            var max = tab[0];
-            for (var i = 0; i < tab.length; i++) {
-                if (typeof tab[i] == "number") {
-                    if (tab[i] > max) {
-                        max = tab[i];
-                    }
-                } else {
-                    return false;
-                }
-            }
-            return max;
-
-        },
-
-        //Napisz funckje podmieniająca wybrany string w tablicy stringów
-        swapString: function (tab, stringToSwap, stringToAdd) {
-            for (var i = 0; i < tab.length; i++) {
-                if (tab[i] == stringToSwap) {
-                    tab[i] = stringToAdd;
-                    return tab;
-                }
-            }
-            return false;
-
-        },
-
-        //Napisz funkcje zwracajaca obiekt o określonej nazwie z tablicy obiektów
-        getObject: function (list, name) {
-            for (var i = 0; i < list.length; i++) {
-                if (list[i].name == name) {
-                    return list[i];
-                }
-            }
-            return false;
-
-        },
-
-        //Napisz funkcje modyfikujaca liste osob w taki sposób, że osoba ponizej 18 lat zostaje wpisane 18.
-        //Pozostale informacje są niemodyfikowane
-        doAdult: function (list) {
-            for (var i = 0; i < list.length; i++) {
-                if (list[i].age < 18) {
-                    list[i].age = 18;
-                }
-            }
-        },
-
         //Napisz funckje przyjmująca 3 parametry która z tych parametrów będzie tworzyć obiekt.
 
         createArrayObject: function (obj1, obj2, obj3) {
@@ -202,50 +61,12 @@
             return object;
 
 
-        },
-
-        diffqube: function (a, b) {
-
-            return a * a * a - b * b * b;
 
 
-        },
-
-        przydz: function (wzrost) {
-
-            if (wzrost >= 160) {
-                console.log("Jestes niskiego wzrostu");
-            }
-            else if (wzrost <= 180) {
-                console.log("Jestes sredniego wzrostu");
-            }
-            else if (wzrost <= 220) {
-                console.log("Jestes Wysoki");
-            }
-        },
-
-
-        fx: function (tab) {
-            for (var i = 0; i >= tab.length; i++) {
-                tab[i] = tab[i] * 3;
-                return tab[i];
-            }
-
-        },
-
-        names: function (FL) {
-
-            return FL[0] + " " + FL[1];
-        },
-
-        f1: function (a) {
-            return a = a * 2;
-        },
-
-        f2: function (b) {
-            return b = b + "!";
         }
 
 
     };
+
+
 })();
--- test/spec/day2.spec.js
+++ test/spec/day2.spec.js
@@ -1,142 +1,5 @@
-(function () {
+describe('day2', function () {
     var answer = window.day2;
-
-    describe('findTheBiggerBox', function () {
-        iit('should return 2 (#box)', function () {
-            expect(answer.findTheBiggerBox(8, 6, 5)).toEqual(2);
-        });
-        iit('should return 1 (#box)', function () {
-            expect(answer.findTheBiggerBox(5, 1, 1)).toEqual(1);
-        });
-        iit('should return false for string value (#box)', function () {
-            expect(answer.findTheBiggerBox(9, "string1", 2)).toBeFalsy();
-        });
-
-
-    });
-    describe('makePerson', function () {
-        iit('should return object Dawid Zegar', function () {
-            expect(answer.makePerson("Dawid", "Zegar", 23)).toEqual({firstName: "Dawid", lastName: "Zegar", age: 23});
-        });
-        iit('should return object Jan Kowalski', function () {
-            expect(answer.makePerson("Jan", "Kowalski", 44)).toEqual({firstName: "Jan", lastName: "Kowalski", age: 44});
-        });
-
-
-    });
-    describe('invertString', function () {
-
-        iit('should return 1tseT', function () {
-            expect(answer.invertString("Test1")).toEqual("1tseT");
-        });
-        iit('should return false', function () {
-            expect(answer.invertString(234)).toBeFalsy();
-        });
-
-
-    });
-    describe('factorial', function () {
-
-        iit('should return [1, 1]', function () {
-            expect(answer.factorial(1)).toEqual([1, 1]);
-        });
-        iit('should return 6 element = 720', function () {
-            expect(answer.factorial(7)[6]).toEqual(720);
-        });
-
-    });
-    describe('sumObject', function () {
-
-        iit('should return 38', function () {
-            expect(answer.sumObject({param0: 2, param1: 5, param2: 9, param3: 2, param4: 20})).toEqual(38);
-        });
-
-
-    });
-    describe('multiplyElementOfArray', function () {
-
-        it('should array where elements with odd index are multiply by 4 and even index element are multiply by 3', function () {
-            expect(answer.multiplyElementOfArray([2, 3, 4, 5])).toEqual([6, 12, 12, 20]);
-        });
-
-        it('should return unchanged array of string when input is a string array', function () {
-            expect(answer.multiplyElementOfArray(["a", "b", "c", "d"])).toEqual(["a", "b", "c", "d"]);
-        });
-
-        it('should return  array with multipli numbers elements and unchanged string element when input is a string and number array', function () {
-            expect(answer.multiplyElementOfArray(["a", 2, "c", 3])).toEqual(["a", 8, "c", 12]);
-        });
-
-    });
-
-    describe('maxArray', function () {
-
-        it('should return max element from array', function () {
-            expect(answer.maxArray([2, 5, 7, 1, 3, 20, 10, 19])).toEqual(20);
-        });
-
-        it('should return false if array not contain numbers ', function () {
-            expect(answer.maxArray(["a", "b", "c"])).toBeFalsy();
-        });
-
-        it('should return false when array contain numbers and other elements', function () {
-            expect(answer.maxArray(["a", 7, "c", 3, "ala", "jankowalski", 10])).toBeFalsy();
-        });
-    });
-
-    describe('swapString', function () {
-
-        it('should swap string in array', function () {
-            expect(answer.swapString(["ala", "ma", "kota"], "ala", "marysia")).toEqual(["marysia", "ma", "kota"]);
-        });
-        it('should return false if stringToSwap not exist in array', function () {
-            expect(answer.swapString(["ala", "ma", "kota"], "marysia", "kot")).toBeFalsy();
-        });
-    });
-
-    describe('getObject', function () {
-
-        var list = [
-            {name: "Jan", age: 10},
-            {name: "Ala", age: 14},
-            {name: "Andrzej", age: 30},
-            {name: "Maria", age: 55}
-        ];
-
-        it('should return object from list', function () {
-            expect(answer.getObject(list, "Ala")).toEqual({name: "Ala", age: 14});
-        });
-
-        it('should return false if object not exist', function () {
-            expect(answer.getObject(list, "Rafał")).toBeFalsy();
-        });
-    });
-
-    describe('doAdult', function () {
-
-        var list = [
-            {name: "Jan", age: 10},
-            {name: "Ala", age: 14},
-            {name: "Andrzej", age: 30},
-            {name: "Maria", age: 55}
-        ];
-        var teenAge = list[0].age;
-        var adultAge = list[3].age;
-
-        beforeEach(function () {
-            answer.doAdult(list);
-        });
-        iit('should change person age', function () {
-            expect(list[0].age).not.toEqual(teenAge);
-        });
-        iit('should change person age to 18 if age<18', function () {
-            expect(list[1].age).toEqual(18);
-        });
-        iit('should dont change age if aga>18', function () {
-            expect(list[3].age).toEqual(adultAge);
-        });
-
-    });
 
     describe('createArrayObject', function () {
         iit('should return array object numeric', function () {
@@ -177,59 +40,17 @@
 
         });
     });
-
     describe("addPropertiesToObjectCheckNumber", function () {
         iit('should return change object input value number', function () {
-            expect(answer.addPropertiesToObjectCheckNumber({properties1: 20, properties2: 30}, "properties3", '1234')).toEqual({properties1: 20, properties2: 30, properties3: 1234});
+            expect(answer.addPropertiesToObjectCheckNumber({properties1: 20, properties2: 30}, "properties3", 1234)).toEqual({properties1: 20, properties2: 30, properties3: 1234});
 
         });
-
 
         iit("should return change object input string asd", function () {
             expect(answer.addPropertiesToObjectCheckNumber({properties1: 20, properties2: 30}, 'properties3', 'asd')).toEqual({properties1: 20, properties2: 30, properties3: 'asd'});
 
         });
-
-
-    });
-
-    describe("diffqube", function () {
-        iit('Napisz funkcje ktora oblicza roznice dwoch szescianow a oraz b. wyswietl wynik w konsoli', function () {
-            expect(answer.diffqube(2, 3).toEqual(-19));
-        });
-    });
-
-    describe("przydz", function () {
-        iit('Napisac funcje ktora przyjmuje wzrost osoby(od 150 cm do 220) i przydziela osoby do odpowiedniej kategorii', function () {
-            expect(answer.przydz(160).toEqual("Jestes niskiego wzrostu"));
-
-        });
-    });
-    describe("fx", function () {
-        iit('Zadeklarowac dowolna macierz cyfr i pomnozyc kazdy el. przed 3.', function () {
-        expect(answer.fx([3, 6, 1]).toEqual([9, 18, 3]));
-    });
     });
 
 
-    describe("names", function () {
-        iit('Napisac funkcje ktora skleja stringi w poniższej podmacierzy', function () {
-        expect(answer.names(["Jan", "Kowalski"]).toEqual(["Jan Kowalski"]));
-    });
-    });
-
-
-    describe("addPropertiesToObjectCheckNumber", function () {
-    iit('Zadeklaruj tablice 2 funkcji.1 funkcja mnozy argument x2 druga funkcja przeksztalca argument na silnie', function () {
-        expect(answer.f1(5).toEqual(10));
-    });
-    });
-
-    describe("addPropertiesToObjectCheckNumber", function () {
-    iit('Factorial ', function () {
-        expect(answer.f2(6).toEqual('6!'));
-    });
-    });
-
-
-})();
+});
