21476b8220afce55847e1c0fe178f982fb2e5b1b
Kronos66 <maksymilian.pamula@gmail.com>
2014-08-25 13:15:59 +0200

    update day3 Max

--- impl/day3.js
+++ impl/day3.js
@@ -22,8 +22,28 @@
         }
         str = str.substring(0, str.length - 1);
         return str;
-
     };
+
+
+//    Napisz funckje do prototypu Number która wywołana na rzecz liczby doda do niej parametr podany w funckji
+    Number.prototype.addNumber = function (add) {
+        return this + add;
+    };
+
+
+//    Napisz funckje ktora będzie wywołana na rzecz obiektu typu number, będzie ona przyjmować dwa argumenty
+//    i zwracać warość najœiekszą z dwóch argumentów i obiektu na rzecz którego została wywołana
+    Number.prototype.maxValueFromTwoArguments = function (first, second) {
+        if (this > first && this > second) {
+            return this;
+        } else if (first > this && first > second) {
+
+            return first;
+        } else if (second > this && second > first) {
+
+            return second;
+        }
+    }
 
     window.day3 = {
         // Dopisz funkcje w srodku obiektu usuwajaca z podanego obiektu wybrany atrybut i zwracajacy zmodyfikowany obiekt
@@ -38,10 +58,35 @@
                 delete this[property];
                 return this;
             }
+        },
+        aqurium: {
+            fish1: {type: 'fish', name: 'Bibo', weight: 20, sizeFish: 'big' },
+            fish2: {type: 'fish', name: "Bagins", weight: 5, sizeFish: 'small'},
+            fish3: {type: 'fish', name: "Bagins", weight: 5, sizeFish: 'small'},
+            fish4: {type: 'fish', name: "Bagins", weight: 5, sizeFish: 'small'},
+            fish5: {type: 'fish', name: "Bagins", weight: 5, sizeFish: 'small'},
+            aqurine: 1,
+            underwaterTree1: {type: 'tree', height: 5},
+            underwaterTree2: {type: 'tree', height: 2},
+            underwaterTree3: {type: 'tree', height: 2},
+            underwaterTree4: {type: 'tree', height: 2},
+            underwaterTree5: {type: 'tree', height: 2},
+            underwaterTree6: {type: 'tree', height: 2},
+            stone1: {type: 'stone', height: 2},
+            stone: 50
 
         },
+        aqurium2: {
+            something: 2,
+            width: 2,
+            height: 20,
+            capacity: 30
 
-        // Napisz funkcje przyjmujaca dowolny obiekt i zwrawacaja w tablicy stringow nazwy atrybutow danego obiektu oraz ich wartosc
+        },
+        test: {
+            properties: true
+        },
+//          Napisz funkcje przyjmujaca dowolny obiekt i zwrawacaja w tablicy stringow nazwy atrybutow danego obiektu oraz ich wartosc
         propertyNames: function (obj) {
             var properties = [];
             for (var prop in obj) {
@@ -49,6 +94,7 @@
             }
             return properties;
         },
+
 
         // Napisz funkcje laczaca dwa dowolne obiekty (wszystkie atrybuty w jednym obiekcie)
         mergeObject: function (obj1, obj2) {
@@ -60,8 +106,42 @@
             }
 
             return obj1;
+        },
+//        Napisz funckje która bęðzie wypisywać ilosc elemtnow w obiekcie jako parametr przyjmuje obiekt i nazwe wlasciwosci
+
+        countPropertiesInObject: function (object, nameProperties) {
+            var count = 0;
+            for (var fish in object) {
+                if (object[fish].type == nameProperties) {
+                    count++;
+                }
+            }
+            return count;
+        },
+//        Napisz funckje ktora wpisze wszystkie nazwy elementow obiektu do tablicy
+
+        writeAllPropertiesObject: function (object) {
+            var temp = [];
+            for (var properties in object) {
+                temp.push(properties);
+            }
+            return temp;
+        },
+
+//        Napisz funkcje ktora zlicza ilosc wystapienia litery w nazwach atrybutów
+        countCharInProperties: function (object, letter) {
+            var temp = 0;
+            for (var properties in object) {
+                for (var i = 0; i < properties.length; i++) {
+                    if (properties.charAt(i).toUpperCase() == letter.toUpperCase()) {
+                        temp++;
+                    }
+                }
+            }
+            return temp;
         }
 
+    }
 
-    };
+
 })();
