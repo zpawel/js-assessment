76df3e6c2e946aa91497c379b41b5adf6b10413f
Dawid Zegar <davvvid1@gmail.com>
2014-09-01 12:11:20 +0200

    modify day 8 Dawid Zegar

--- impl/day8.js
+++ impl/day8.js
@@ -41,4 +41,41 @@
 
     var module = angular.module('day8App', []);
     module.controller('ShoppingCartCtrl', [ShoppingCartCtrl]);
+
+
+    var exerciseCtrl = function ($scope) {
+        this.personsList = [];
+        this.person = { firstName: $scope.firstName,
+            lastName: $scope.lastName,
+            pesel: $scope.pesel,
+            email: $scope.email
+        }
+        this.checkEmail = function () {
+            if (this.person.email.indexOf("@") > -1) {
+                return true;
+            }
+            else {
+                return false;
+            }
+
+        }
+        this.checkPesel = function () {
+            var pes = this.person.pesel;
+            if (pes.length == 11 && ( parseInt(pes[0]) + 3 * parseInt(pes[1]) + 7 * parseInt(pes[2]) + 9 * parseInt(pes[3]) + parseInt(pes[4]) + 3 * parseInt(pes[5]) + 7 * parseInt(pes[6]) + 9 * parseInt(pes[7]) + parseInt(pes[8]) + 3 * parseInt(pes[9]) + parseInt(pes[10]) ) % 10 == 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        this.addPerson = function () {
+            if (this.checkPesel() && this.checkEmail()) {
+                this.personsList.push(this.person);
+            }
+        }
+    }
+
+    module.controller('exerciseCtrl', ['$scope', exerciseCtrl]);
+
 })();
