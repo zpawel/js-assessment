7e44a32d2fcefa09693018da4a50058d6ac4488c
zpawel <zpawel92@gmail.com>
2015-07-20 13:04:58 +0200

    day1

--- impl/day1.js
+++ impl/day1.js
@@ -22,36 +22,46 @@
                        return false;
                },
 
-//    function deleteStr(stringToDelete, arrayOfStrings){
-//        var i;
-//        for(i=0;i<arrayOfStrings.length;i++){
-//            if (arrayOfStrings[i]==stringToDelete)
-//        		arrayOfStrings.splice(i,1);
-//        }
-//    },
-//
-//    function stringCounter(customArray){
-//        var x = 0;
-//        for (var obj of customArray){
-//            if(typeof(obj) == 'string')
-//                a++;
-//        }
-//        console.log(x);
-//    },
-//
-//    function squareOdd(customArray){
-//    	var i=0;
-//    	for(i=0;i<customArray.length;i++){
-//    		if(customArray[i]%2 == 0){
-//    			customArray[i] = Math.pow(customArray[i],2)
-//    		}
-//    	}
-//       	console.log(customArray);
-//    },
-//
-//    function trapezeSquare(a,b,h){
-//    	var x = h * ( a + b ) / 2;
-//    	console.log(x);
-// }
+    deleteStr: function(stringToDelete, arrayOfStrings)
+         {
+
+                      var i;
+                               for(i=0;i<arrayOfStrings.length;i++){
+                                 if (arrayOfStrings[i]==stringToDelete)
+                                 arrayOfStrings.splice(i,1);
+                                   } return arrayOfStrings;
+                               },
+
+    stringCounter: function(customArray){
+        var x = 0;
+        var i;
+        for(i=0;i<customArray.length;i++){
+            if(typeof(customArray[i]) === 'string')
+                x++;
+        }
+        return(x);
+    },
+
+    squareOdd: function (customArray){
+    	var i=0;
+    	for(i=0;i<customArray.length;i++){
+    		if(customArray[i]%2 == 1){
+    			customArray[i] = Math.pow(customArray[i],2)
+    		}
+    	}
+       	return(customArray);
+    },
+
+    trapezeSquare: function (a,b,h){
+    	var x = h * ( a + b ) / 2;
+    	if (typeof(a)=='string' || typeof(b)=='string' || typeof(h)=='string' || a<0 || b<0 || h<0)
+    	{
+        	return false;
+    	}
+    	else
+    	{
+        	return(x);
+        }
+ }
    }
 })();
