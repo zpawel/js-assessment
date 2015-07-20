234cfb2339e292290f0d3629f7f85c62e2a9e10a
zpawel <zpawel92@gmail.com>
2015-07-20 11:58:03 +0200

    day1

--- impl/day1.js
+++ impl/day1.js
@@ -6,42 +6,46 @@
     function getDescendingNumbers(numberFrom, numberTo){
         if(numberFrom>numberTo){
             var i;
+            var j;
             for (i=numberFrom;i>=numberTo;i--)
-                console.log(i + " ");
+            {
+               j = (i);
+              }
+               return j;
         }else
-            console.log("numberFrom powinno byæ wiêksze od nubmerTo");
+            return ("numberFrom powinno byæ wiêksze od nubmerTo");
     }
 
-    function deleteStr(stringToDelete, arrayOfStrings){
-        var i;
-        for(i=0;i<arrayOfStrings.length;i++){
-            if (arrayOfStrings[i]==stringToDelete)
-        		arrayOfStrings.splice(i,1);
-        }
-    }
-
-    function stringCounter(customArray){
-        var x = 0;
-        for (var obj of customArray){
-            if(typeof(obj) == 'string')
-                a++;
-        }
-        console.log(x);
-    };
-
-    function squareOdd(customArray){
-    	var i=0;
-    	for(i=0;i<customArray.length;i++){
-    		if(customArray[i]%2 == 0){
-    			customArray[i] = Math.pow(customArray[i],2)
-    		}
-    	}
-       	console.log(customArray);
-    }
-
-    function trapezeSquare(a,b,h){
-    	var x = h * ( a + b ) / 2;
-    	console.log(x);
-    }
-})();
-
+//    function deleteStr(stringToDelete, arrayOfStrings){
+//        var i;
+//        for(i=0;i<arrayOfStrings.length;i++){
+//            if (arrayOfStrings[i]==stringToDelete)
+//        		arrayOfStrings.splice(i,1);
+//        }
+//    }
+//
+//    function stringCounter(customArray){
+//        var x = 0;
+//        for (var obj of customArray){
+//            if(typeof(obj) == 'string')
+//                a++;
+//        }
+//        console.log(x);
+//    };
+//
+//    function squareOdd(customArray){
+//    	var i=0;
+//    	for(i=0;i<customArray.length;i++){
+//    		if(customArray[i]%2 == 0){
+//    			customArray[i] = Math.pow(customArray[i],2)
+//    		}
+//    	}
+//       	console.log(customArray);
+//    }
+//
+//    function trapezeSquare(a,b,h){
+//    	var x = h * ( a + b ) / 2;
+//    	console.log(x);
+//    }
+//})();
+//
